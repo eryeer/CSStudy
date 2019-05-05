ce9dd9fda464d189b50bd70d6f7604178578e3e8
eryeer <eryeer@163.com>
2019-05-05 22:06:42 +0800

    init

---
+++ HelloWorld/DataType/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int a = 3;
+            Console.WriteLine("{0}",a.ToString());
+           
+
+            string hello = "hello";
+
+            string c = Console.ReadLine();
+            Console.WriteLine("{0} world is {1}", hello, int.Parse(c) + a);
+            Console.ReadLine();
+
+        }
+    }
+}
